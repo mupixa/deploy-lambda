3f5af5e1441c1f782c63b13bc37ae87438169c4e
vldaskalov <vld.daskalov@gmail.com>
2020-08-30 20:28:08 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# deploy-lambda
-Deploy a lambda function from a zip and update it's configuration
+Github Action to update a Lambda function from a ZIP file.
+
+## Required parameters
+
+- `ZIP`: A ZIP file containing with the source of the lambda.
+- `FUNCTION_NAME`
+- `AWS_REGION`
+- `AWS_SECRET_ID`
+- `AWS_SECRET_KEY`
+- `RUNTIME`
+- `ROLE`
+- `HANDLER`
+- `DESCRIPTION`
+- `TIMEOUT`
+- `MEMORY_SIZE`
+- `INVIRONMENT`
+
+## Example
+
+```
+name: Deploy AWS Lambda
+
+on:
+  pull_request:
+    types: [merged]
+      branches:
+        - master
+
+jobs:
+  deploy-lambda:
+    if: github.event.pull_request.merged
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@master
+      - run: echo "GENERATE/FETCH ZIP"
+      - uses: mupixa/deploy-lambda
+        with:
+          package: TEST-LAMBDA-ZIP.zip
+          function-name: TEST-FUNCTION-NAME
+          AWS_REGION: ${{ secrets.AWS_REGION }}
+          AWS_SECRET_ID: ${{ secrets.AWS_SECRET_ID }}
+          AWS_SECRET_KEY: ${{ secrets.AWS_SECRET_KEY }}
+```
---
+++ index.js
@@ -0,0 +1,102 @@
+// const github = require('@actions/github');
+const core = require("@actions/core");
+const fs = require("fs");
+const AWS = require("aws-sdk");
+
+try {
+  // Get all parameters
+  const ZIP = core.getInput("ZIP");
+  const FUNCTION_NAME = core.getInput("FUNCTION_NAME");
+  const AWS_REGION = core.getInput("AWS_REGION");
+  const AWS_SECRET_KEY = core.getInput("AWS_SECRET_KEY");
+  const AWS_SECRET_ID = core.getInput("AWS_SECRET_ID");
+  const RUNTIME = core.getInput("RUNTIME");
+  const ROLE = core.getInput("ROLE");
+  const HANDLER = core.getInput("HANDLER");
+  const DESCRIPTION = core.getInput("DESCRIPTION");
+  const TIMEOUT = core.getInput("TIMEOUT");
+  const MEMORY_SIZE = core.getInput("MEMORY_SIZE");
+  const ENVIRONMENT = core.getInput("ENVIRONMENT");
+
+  // Check params
+  if (!ZIP) {
+    throw "No ZIP provided!";
+  }
+  if (!FUNCTION_NAME) {
+    throw "No FUNCTION_NAME provided!";
+  }
+  if (!AWS_REGION) {
+    throw "No AWS_REGION provided!";
+  }
+  if (!AWS_SECRET_KEY) {
+    throw "No AWS_SECRET_KEY provided!";
+  }
+  if (!AWS_SECRET_ID) {
+    throw "No AWS_SECRET_ID provided!";
+  }
+
+  console.log(`Deploying ${FUNCTION_NAME} from ${ZIP}.`);
+
+  const zipBuffer = fs.readFileSync(`./${ZIP}`);
+  core.debug("ZIP read into memory.");
+
+  const lambda = new AWS.Lambda({
+    apiVersion: "2015-03-31",
+    region: AWS_REGION,
+    secretAccessKey: AWS_SECRET_KEY,
+    accessKeyId: AWS_SECRET_ID,
+    maxRetries: 3,
+    sslEnabled: true,
+    logger: console,
+  });
+
+  const uploadParams = {
+    FunctionName: FUNCTION_NAME,
+    Publish: true,
+    ZipFile: zipBuffer,
+  };
+
+  lambda.updateFunctionCode(uploadParams, (err) => {
+    if (!!err) {
+      console.error(err);
+      core.setFailed(err);
+    }
+  });
+
+  let configParams = {
+    FunctionName: FUNCTION_NAME,
+  };
+  if (!!RUNTIME) {
+    configParams["Runtime"] = RUNTIME;
+  }
+  if (!!ROLE) {
+    configParams["Role"] = ROLE;
+  }
+  if (!!HANDLER) {
+    configParams["Handler"] = HANDLER;
+  }
+  if (!!DESCRIPTION) {
+    configParams["Description"] = DESCRIPTION;
+  }
+  if (!!TIMEOUT) {
+    configParams["Timeout"] = TIMEOUT;
+  }
+  if (!!MEMORY_SIZE) {
+    configParams["MemorySize"] = MEMORY_SIZE;
+  }
+  if (!!ENVIRONMENT) {
+    const Variables = JSON.parse(ENVIRONMENT);
+    configParams["Environment"] = { Variables };
+  }
+
+  if (Object.keys(configParams).length > 1) {
+    lambda.updateFunctionConfiguration(configParams, (err) => {
+      if (!!err) {
+        console.error(err);
+        core.setFailed(err);
+      }
+    });
+  }
+} catch (error) {
+  core.setFailed(error.message);
+}
